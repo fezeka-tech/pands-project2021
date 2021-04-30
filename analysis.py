5608c72c3947923f285c2455176432f82c646c65
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-04-30 15:49:46 +0100

    Create analysis.py
    Code for program that summarises Iris Data

---
+++ analysis.py
@@ -0,0 +1,12 @@
+// This program outputs a summary of each variable to a single text file//
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+from sklearn.datasets import load_iris
+Iris= load_iris
+
+Iris.data.shape
