89a1f57a76fbdbd5c600617a1468f95f2b40a669
Fezeka Nzimande <60849112+fezeka-tech@users.noreply.github.com>
2021-05-06 23:11:04 +0100

    Update analysis.py
    Update program with scatterplot code, an additional edits

--- analysis.py
+++ analysis.py
@@ -1,8 +1,9 @@
-// This program outputs a summary of each variable to a single text file//
+# This program outputs a summary of each variable to a single text file #
 
 import pandas as pd
+import numpy as np
 
-irisData= irisdata.scv #work on chanhing file to csv for analysis#
+irisData= irisdata.csv
 
 df = pd.DataFrame(irisData, columns = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
 print (df.head(5))
@@ -13,7 +14,7 @@ print(type(df.decsribe()))
 
 
 
-#saves a histogram of each variable to png files# 
+# Saves a histogram of each variable to png files # 
 
 import pandas as pd
 import numpy as np
@@ -23,4 +24,20 @@ from sklearn.datasets import load_iris
 
 df.plot.histogram
 
-plt.show(). # finalise and check does code run#
+plt.show(). 
+
+plt.savefig().   #saves the histogram to a png file# 
+
+
+
+# Outputs a scatterplot of each pair of variables #
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+
+df[['sepal length_width', 'petal length_width']].plot.scatterplot  
+
+plt.show()
